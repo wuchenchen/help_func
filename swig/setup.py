3dc85a677147e51b8a28d5774611e381e9a49cc7
wuchenchen <wuchenchen1217@gmail.com>
2020-05-27 14:40:33 +0800

    清理

--- swig/setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-
-from distutils.core import setup, Extension
-
-
-example_module = Extension('_example', sources=['example.c', 'example.i'])
-
-setup(name='example', ext_modules=[example_module], py_modules=["example"])
